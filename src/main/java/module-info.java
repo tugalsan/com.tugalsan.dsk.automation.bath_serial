a53c75d9ac9e8a1018eb1d9667777e48db3ecb7e
tugalsan karabacak <tugalsan@gmail.com>
2023-08-13 07:29:34 +0300

    .

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module com.tugalsan.dsk.trainer {
+    requires java.desktop;
+    requires AbsoluteLayout.RELEASE180;
+    requires com.tugalsan.api.log;
+    requires com.tugalsan.api.string;
+    requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.thread;
+    requires com.tugalsan.api.list;
+    requires com.tugalsan.api.stream;
+    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.validator;
+    requires com.tugalsan.api.desktop;
+    requires com.tugalsan.api.coronator;
+    requires com.tugalsan.api.file.properties;
+    requires com.tugalsan.api.file;
+    requires com.tugalsan.api.file.txt;
+    requires com.tugalsan.api.time;
+    requires com.tugalsan.api.cast;
+    requires com.tugalsan.api.runnable;
+    requires com.tugalsan.api.shape;
+    requires com.tugalsan.api.serialcom;
+    requires com.tugalsan.api.serialcom.kincony;
+}
